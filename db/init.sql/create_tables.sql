3d26a1ab2cf49209d19a3e85f045d6d00d8bc10b
stas kloyzner <w8p-a@outlook.com>
2022-07-09 23:33:57 +0000

    Updated create_tables.sql

--- db/init.sql/create_tables.sql
+++ db/init.sql/create_tables.sql
@@ -1,1 +1,6 @@
-measurements
+`CREATE TABLE IF NOT EXISTS measurements (
+			id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+			, user_id varchar(50) NOT NULL
+			, measure_date date NOT NULL
+			, weight numeric(5,1) NOT NULL
+		)`
