22aaa63c905d745ba8cc3911b4ae7c27d5f95074
stas kloyzner <w8p-a@outlook.com>
2022-07-09 17:43:33 +0000

    Updated create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 cat > ./.env << EOL
+
 # Host configuration
+
 PORT=8080
 HOST=0.0.0.0
 HOST_URL=http://$1:8080
 COOKIE_ENCRYPT_PWD=superAwesomePasswordStringThatIsAtLeast32CharactersLong!
 NODE_ENV=development
-          
+
 # Okta configuration
 OKTA_ORG_URL=$2
 OKTA_CLIENT_ID=$3
 OKTA_CLIENT_SECRET=$4
-        
 # Postgres configuration
+
 PGHOST=$5
 PGUSERNAME=$6
 PGDATABASE=postgres
 PGPASSWORD=$7
 PGPORT=5432
+
+POSTGRES_USER=$6
+POSTGRES_PASSWORD=$7
 EOL
